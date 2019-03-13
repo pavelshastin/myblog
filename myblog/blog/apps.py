199835ad14523388a019b9fd4207f40c5eefded5
Павел Шастин <C:\Users\P.Shastin\AppData\Roaming\The Bat!>
2019-03-13 15:24:00 +0300

    f

--- myblog/blog/apps.py
+++ myblog/blog/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-print("BlogConfig")
 
 class BlogConfig(AppConfig):
     name = 'blog'
--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -40,11 +40,11 @@ class Entry(models.Model):
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
     headline = models.CharField(max_length=200)
     body_text = models.TextField()
-    pub_date = models.DateField()
-    mod_date = models.DateField()
+    pub_date = models.DateField(default=timezone.now)
+    mod_date = models.DateField(default=timezone.now)
     authors = models.ManyToManyField(Author)
-    n_comments = models.IntegerField()
-    n_pingback = models.IntegerField()
+    n_comments = models.IntegerField(default=10)
+    n_pingback = models.IntegerField(default=10)
     rating = models.IntegerField()
 
     def __str__(self):
