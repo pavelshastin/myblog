0eaa18578f1c44d8e2b3dfd6b741535de1f099e0
Павел Шастин <C:\Users\P.Shastin\AppData\Roaming\The Bat!>
2019-03-13 14:32:40 +0300

    django

---
+++ myblog/blog/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+print("BlogConfig")
+
+class BlogConfig(AppConfig):
+    name = 'blog'
+
---
+++ myblog/blog/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+print("models")
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=300)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(default=timezone.now)
+
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+
+class Blog(models.Model):
+    name = models.CharField(max_length=200)
+    tagline = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+
+class Entry(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    headline = models.CharField(max_length=200)
+    body_text = models.TextField()
+    pub_date = models.DateField()
+    mod_date = models.DateField()
+    authors = models.ManyToManyField(Author)
+    n_comments = models.IntegerField()
+    n_pingback = models.IntegerField()
+    rating = models.IntegerField()
+
+    def __str__(self):
+        return self.headline
+
+
+
+
