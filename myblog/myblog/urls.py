fed2fd465a234dc70944d525dc8d146efdd6ee69
Павел Шастин <C:\Users\P.Shastin\AppData\Roaming\The Bat!>
2019-03-15 15:40:02 +0300

    f

--- myblog/myblog/urls.py
+++ myblog/myblog/urls.py
@@ -20,6 +20,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^$', include('blog.urls')),
+    url(r'', include('blog.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+] #+ static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
