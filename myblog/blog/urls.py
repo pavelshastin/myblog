cf57f20f1fda339481ce0ca1e32134be11de6ca2
Павел Шастин <C:\Users\P.Shastin\AppData\Roaming\The Bat!>
2019-03-19 14:59:37 +0300

    year-month selection

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('posts/<int:post_id>', views.detail, name='detail')
+    path('reciepts/<int:post_id>', views.detail, name='detail'),
+    path('reciepts/year/<int:year>', views.reciepts_year, name="reciepts_year"),
+    path('reciepts/year/<int:year>/month/<int:month>', views.reciepts_month, name="reciepts_month")
 ]
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.http import HttpResponse
 from .models import Post, Comment
+from django.utils import timezone
+import datetime
 
 def index(request):
     last_reciepts = Post.objects.order_by('-published_date')[0:12]
@@ -13,10 +15,25 @@ def index(request):
     return render(request, "blog/food-index.html", context)
 
 def detail(request, post_id=1):
-    print(post_id)
-    context = {}
-    return render(request, 'blog/food-single.html', context)
+    post = get_object_or_404(Post, pk=post_id)
+
+    return render(request, 'blog/food-index.html', {'post': post})
 
 
 
+def reciepts_year(request, year=2019):
 
+    reciepts = get_list_or_404(Post, modified_date__year=year)
+    months = {}
+
+    for rec in reciepts:
+        month_str = rec.modified_date.strftime("%B")
+        month_num = rec.modified_date.strftime("%m")
+        months.update({month_str: month_num})
+
+
+    print("months", months)
+    return render(request, 'blog/food-index.html', {'year': year, 'months': months})
+
+def reciepts_month(request, year=2019, month=0):
+    pass
