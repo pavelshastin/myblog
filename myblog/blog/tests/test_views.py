9585a50d7c20416aa89e8c0b480d1a3965f9ac2b
Павел Шастин <C:\Users\P.Shastin\AppData\Roaming\The Bat!>
2019-03-26 14:19:24 +0300

    tests for model and views

--- myblog/blog/tests/test_views.py
+++ myblog/blog/tests/test_views.py
@@ -3,7 +3,7 @@ from django.utils import timezone as tz
 from django.urls import reverse
 
 from blog.models import Post, Comment
-n = 0
+
 
 class ViewTestCase(TestCase):
     fixtures = ['dump.json']
@@ -11,10 +11,8 @@ class ViewTestCase(TestCase):
     def setUp(self):
         self.client = Client()
         self.last_post = Post.objects.filter(modified_date__lte=tz.now()).order_by('-modified_date')[0]
+        self.posts_year_2019 = Post.objects.filter(modified_date__year=2019)
 
-        global n
-        print(n, self.last_post)
-        n+=1
 
     def test_index(self):
         resp = self.client.get(reverse('index'))
@@ -25,3 +23,17 @@ class ViewTestCase(TestCase):
     def test_list_of_reciepts_for_2019(self):
         resp = self.client.get('/reciepts/year/2019')
         self.assertEqual(resp.status_code, 200)
+        self.assertContains(resp, '2019')
+
+        months = set([p.modified_date.month for p in self.posts_year_2019])
+        self.assertEqual(len(months), len(resp.context["months"]))
+
+
+    def test_list_of_reciepts_for_Jan_2019(self):
+        resp = self.client.get('/reciepts/year/2019/month/01')
+        self.assertEqual(resp.status_code, 200)
+        self.assertContains(resp, 'January')
+
+        jan = Post.objects.filter(modified_date__month=1)
+        self.assertEqual(len(jan), len(resp.context["month_reciepts"]))
+
