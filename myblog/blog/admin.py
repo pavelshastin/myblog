f805c7d9fabd3ae6a27cfb8cb8348fb04fa4d7cd
pavelshastin <pavelshastin@yandex.ru>
2019-03-31 18:14:14 +0400

    admin customizing

--- myblog/blog/admin.py
+++ myblog/blog/admin.py
@@ -2,9 +2,35 @@ from django.contrib import admin
 from .models import Post, Comment, MyUser
 from django.contrib.auth.admin import UserAdmin
 
+class CommentsInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+
+
+class PostInline(admin.StackedInline):
+    model = Post
+    extra = 0
+    fields = (('author', 'title'), 'text', 'modified_date')
+    show_change_link = True
+
+
+
+class PostAdmin(admin.ModelAdmin):
+    fields = (('author', 'title'), 'text', 'modified_date')
+    inlines = (CommentsInline,)
+
+
+class UserAdmin(admin.ModelAdmin):
+    fields = ('username', ('first_name', 'last_name'))
+    inlines = (PostInline, )
+
+class CommentAdmin(admin.ModelAdmin):
+    show_change_link = True
+
+
 admin.site.register(MyUser, UserAdmin)
-admin.site.register(Post)
-admin.site.register(Comment)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
 
 
 # admin.site.register(Author)
